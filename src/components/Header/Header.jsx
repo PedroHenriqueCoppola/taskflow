b6a5a0ad160e2af1e71c4cae7b7df94ba30357a4
Pedro Coppola <89110983+PedroHenriqueCoppola@users.noreply.github.com>
2026-04-14 13:09:09 -0300

    implementing header infos and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import './Header.css';
 
+import { PanelLeft, User } from "lucide-react";
+
 const Header = ({ toggleSidebar }) => {
     return (
         <div className="headerLayout">
-            <button className="menuButton" onClick={toggleSidebar}>
-                ☰
+            <button className="sidebarButton" onClick={toggleSidebar}>
+                <PanelLeft size={16} />
+            </button>
+
+            <button className="userInfos">
+                <User size={16} />
             </button>
         </div>
     )
